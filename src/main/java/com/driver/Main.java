8ffb98c5e46f2f00026f3686e1b0a46280cb1dc8
Harshvardhan1909 <harsh970919@gmail.com>
2022-12-02 20:45:01 +0530

    first commit

---
+++ src/main/java/com/driver/Main.java
@@ -0,0 +1,23 @@
+package com.driver;
+
+public class Main {
+  public static void main(String[] args) {
+    DeluxePizza dp = new DeluxePizza(true);
+    System.out.println(dp.getPrice());
+    dp.addExtraCheese();
+    dp.addExtraCheese();
+
+    System.out.println(dp.getBill());
+
+    Pizza p = new Pizza(true);
+    p.addExtraCheese();
+    p.addExtraToppings();
+    p.addTakeaway();
+    p.getBill();
+    p.getBill();
+    System.out.println(p.getBill());
+
+
+
+  }
+}
